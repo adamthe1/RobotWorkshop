2ff42c4c183a545e9dd293ce4628522ed2a780a8
root <root@ziv.localdomain>
2025-07-31 13:54:34 +0300

    mujoco side implementaion

--- SideMuJoCo/action_manager.py
+++ SideMuJoCo/action_manager.py
@@ -3,6 +3,9 @@ class ActionManager:
     def __init__(self, data):
         self.data = data
 
-    def apply_joint_targets(self, joint_targets):
-        self.data.ctrl[:len(joint_targets)] = joint_targets
+    def apply_joint_targets(self,indices, joint_targets):
+        if len(joint_targets) != len(indices):
+             raise ValueError(f"Action length {len(action)} doesn't match robot actuator count {len(indices)}")
+        for i, val in zip(indices, joint_targets):
+          data.ctrl[i] = val
 
